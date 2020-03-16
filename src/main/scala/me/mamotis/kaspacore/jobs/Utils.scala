0991a4015edb276487f217f344ea49814778514a
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2020-03-16 17:56:59 +0700

    fix problem with spark conf
    Signed-off-by: Fadhil Yori Hibatullah <fadhil4812@gmail.com>

--- src/main/scala/me/mamotis/kaspacore/jobs/Utils.scala
+++ src/main/scala/me/mamotis/kaspacore/jobs/Utils.scala
@@ -20,7 +20,6 @@ private[jobs] trait Utils {
       .set("spark.cassandra.auth.password", PropertiesLoader.cassandraPassword)
       .set("spark.cassandra.output.batch.grouping.key", "Partition")
       .set("spark.cassandra.output.concurrent.writes", "2000")
-      .set("spark.mongodb.output.uri", PropertiesLoader.mongodbUri)
 
     val session = SparkSession.builder()
       .config(conf)
